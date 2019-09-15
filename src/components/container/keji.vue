1637c6264230b48d56ba2397dca74f9fa6742d4f
傅诚 <327619852@qq.com>
2019-09-15 20:52:04 +0800

    extract common.css

--- src/components/container/keji.vue
+++ src/components/container/keji.vue
@@ -21,39 +21,5 @@ import mixin from '../mixins/list';
 </script>
 
 <style scoped>
-  .itemList{
-    max-width: 400px;
-    height: 100%;
-    background:  #fafafa;
-    padding: 0 10px;
-  }
-  .table{
-    padding-top: 20px;
-    padding-bottom: 10px;
-    border-bottom: 1px solid rgba(242,242,242,1);
-  }
-  .item-title{
-    font-size: 18px;
-    line-height: 1.7em;
-    margin-bottom: 8px;
-    color:#000;
-  }
-  .item-summary{
-    overflow: hidden;
-    color: #737373;
-    font-size: 16px;
-    line-height: 1.85em;
-    margin-bottom: 24px;
-    /*height: 88px;*/
-    display: -webkit-box;
-    -webkit-box-orient: vertical;
-    -webkit-line-clamp: 3;
-    /*white-space: nowrap;*/
-  }
-  .item-name{
-    color: #999;
-    font-size: 12px;
-    line-height: 1.2em;
-    font-family: PingFangSC-Light,Microsoft YaHei UI Light,Noto Sans CJK SC Light,sans-serif
-  }
+  @import url('./common.css');
 </style>
